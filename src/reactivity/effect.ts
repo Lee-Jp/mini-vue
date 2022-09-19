3e800439339cf0b833ca16e58ab9ef5c88d394ab
flymon <95333@sohu.com>
2022-09-19 09:27:19 +0800

    feat：stop 功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,12 +1,31 @@
+import { extend } from "../shared";
+
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn,public scheduler?) {
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  constructor(fn, public scheduler?) {
     this._fn = fn;
   }
   run() {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 const targetMap = new Map();
 export function track(target, key) {
@@ -21,7 +40,9 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+  if(!activeEffect) return;
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 export function trigger(target, key) {
   let depsMap = targetMap.get(target);
@@ -35,9 +56,16 @@ export function trigger(target, key) {
   }
 }
 let activeEffect;
-export function effect(fn, options:any = {}) {
+export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect,options);
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
