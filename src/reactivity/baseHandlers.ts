06113d582397d3c7e98a1bb6d7becd0f42397685
flymon <95333@sohu.com>
2022-09-24 19:06:52 +0800

    feat：readonly

---
+++ src/reactivity/baseHandlers.ts
@@ -0,0 +1,33 @@
+import { track, trigger } from './effect';
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+    const res = Reflect.get(target, key);
+    if (!isReadonly) {
+      // 依赖收集
+      track(target, key);
+    }
+    return res;
+  };
+}
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    // 触发依赖
+    trigger(target, key);
+    return res;
+  };
+}
+export const mutableHandlers = {
+  get,
+  set,
+};
+export const readOnlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`${key}readonly 不允许 set${value}`)
+    return true;
+  },
+};
