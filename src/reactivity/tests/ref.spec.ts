e94e010eb6c8af0cea18e1f9a7e269e5ad5c1e0d
flymon <95333@sohu.com>
2022-10-23 12:50:52 +0800

    faet：实现 proxyRefs

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,6 +1,6 @@
 import { effect } from '../effect';
 import { reactive } from '../reactive';
-import { ref, isRef,unRef } from '../ref';
+import { ref, isRef, unRef, proxyRefs } from '../ref';
 describe('ref', () => {
   it('happy path', () => {
     const a = ref(1);
@@ -48,10 +48,33 @@ describe('ref', () => {
     expect(isRef(user)).toBe(false);
   });
 
- 
   it('unRef', () => {
     const a = ref(1);
     expect(unRef(a)).toBe(1);
     expect(unRef(1)).toBe(1);
-  }); 
+  });
+
+  it('proxyRefs', () => {
+    const user = {
+      age: ref(10),
+      name: 'xiaohong',
+    };
+    const proxyUser = proxyRefs(user);
+
+    // get => age(ref) 返回.value
+    // not ref => value
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe('xiaohong');
+
+    // set => ref .value
+    // not ref => value
+    proxyUser.age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyUser.age = 10;
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
+  });
 });
