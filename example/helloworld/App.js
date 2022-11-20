6d3cada511910ea3d69a3c763a19a3e6d80a6535
flymon <95333@sohu.com>
2022-11-20 20:46:24 +0800

    feat：初始化 element主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,14 +1,21 @@
 import { h } from '../../lib/guild-mini-vue.esm.js';
 export const App = {
-
-  render(){
-    return h('div', this.msg);
+  render() {
+    return h(
+      'div',
+      {
+        id: 'root',
+        class: ['red', 'hard'],
+      },
+      [h('p', { class: 'red' }, 'hi'), h('p', { class: 'blue' }, 'mini-vue')]
+      // 'hei'
+      // this.msg
+    );
   },
 
-  setup(){
-
+  setup() {
     return {
-      msg:'mini vue',
-    }
-  }
-}
+      msg: 'mini vue',
+    };
+  },
+};
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,10 +1,16 @@
+import { isObject } from '../shared/index';
 import { createComponentInstance, setupComponent } from './components';
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode, container) {
-  processComponent(vnode, container);
+  console.log('vnode :>> ', vnode);
+  if (typeof vnode.type === 'string') {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    processComponent(vnode, container);
+  }
 }
 function processComponent(vnode: any, container: any) {
   mountComponent(vnode, container);
@@ -18,4 +24,28 @@ function mountComponent(vnode: any, container: any) {
 function setupRenderEffect(instance: any, container: any) {
   const subTree = instance.render();
   patch(subTree, container);
+}
+function processElement(vnode: any, container: any) {
+  mountElement(vnode, container);
+}
+function mountElement(vnode: any, container: any) {
+  debugger;
+  const el = document.createElement(vnode.type);
+  const { children, props } = vnode;
+  if (typeof children === 'string') {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(vnode, el);
+  }
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+  container.append(el);
+  // document.append(container);
+}
+function mountChildren(vnode: any, container: any) {
+  vnode.children.forEach((v) => {
+    patch(v, container);
+  });
 }
